802f4dd32046145f726af2885f12ec78527b983f
mkaisuddin <mkaisuddin@gmail.com>
2020-04-21 14:25:45 +0530

    Param Annotations and member variables

--- MessangerAdv/src/main/java/org/kaushik/javabrains/rest/MyResource.java
+++ MessangerAdv/src/main/java/org/kaushik/javabrains/rest/MyResource.java
@@ -2,16 +2,32 @@ package org.kaushik.javabrains.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("test")
+@Path("{pathparam}/test")
 @Produces(MediaType.TEXT_PLAIN)
 public class MyResource {
 	
+	@PathParam("pathparam") private String pathParamExample;
+	@QueryParam("queryparam") private String queryParamExample;
+	
 	@GET
 	public String getTestmethod() {
-		return "MyResource";
+		//http://localhost:8080/MessangerAdv/webapi/paramvalue/test?queryparam=queryvalue
+		return "pathParamExample = " + pathParamExample + "  queryParamExample = " + queryParamExample + "";
 	}
+	
+	/*@GET
+	public String getTestmethod(@PathParam("pathparam") String pathparam) {
+		return "MyResource";
+	}*/
+	
+	/*@GET
+	public String getTestmethod() {
+		return "MyResource";
+	}*/
 
 }
